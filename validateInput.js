a592b24f113b8c320f944beb8082e8613d327a1d
MUHAMMED SHAHID <276502689+shahi845@users.noreply.github.com>
2026-04-25 17:49:05 +0530

    Delete validateInput.js

--- validateInput.js
+++
@@ -1,5 +0,0 @@
-export function validateInput(heirs) {
-    if (heirs.husband > 0 && heirs.wife > 0) {
-        throw new Error("Invalid input: Cannot have both husband and wife as heirs.");
-    }
-}
